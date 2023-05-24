85fc5495cfca2cda5c0f745456b90829621de2bf
Viktoria Vinogradova <vinovinka@yandex.ru>
2023-05-24 14:51:25 +0300

    Added optimisation

--- FizzBuzz.playground/Contents.swift
+++ FizzBuzz.playground/Contents.swift
@@ -6,7 +6,7 @@ import UIKit
 //
 func printFizzBuzz() {
     for i in 1...100 {
-        if i % 3 == 0 && i % 5 == 0 {
+        if i % 15 == 0 {
             print("FizzBuzz")
         } else if i % 3 == 0 {
             print("Fizz")
